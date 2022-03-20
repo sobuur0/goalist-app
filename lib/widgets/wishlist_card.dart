2a6b6b0e28ed74c8a4ebdd596b68ae121fe783aa
t-bolt <isholasobuur@gmail.com>
2022-03-20 18:58:48 +0100

    congratulations_page now shows only after the user has marked a goal as finished

--- lib/widgets/wishlist_card.dart
+++ lib/widgets/wishlist_card.dart
@@ -17,67 +17,69 @@ class WishlistCard extends StatelessWidget {
   Widget build(BuildContext context) {
     final size = MediaQuery.of(context).size;
 
-    return InkWell(
-      onTap: onTap,
-      child: Container(
-        margin: const EdgeInsets.all(10.0),
-        decoration: BoxDecoration(
-          borderRadius: BorderRadius.circular(12.0),
-          color: kWishlistCardColor,
-        ),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.stretch,
-          children: <Widget>[
-            ClipRRect(
-              borderRadius: const BorderRadius.only(
-                topLeft: Radius.circular(12.0),
-                topRight: Radius.circular(12.0),
-              ),
-              child: Image.asset(
-                'images/clock.jpg',
-                fit: BoxFit.fill,
-                height: 100,
-              ),
+    return Container(
+      margin: const EdgeInsets.all(10.0),
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(12.0),
+        color: kWishlistCardColor,
+      ),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: <Widget>[
+          ClipRRect(
+            borderRadius: const BorderRadius.only(
+              topLeft: Radius.circular(12.0),
+              topRight: Radius.circular(12.0),
             ),
-            Padding(
-              padding: const EdgeInsets.symmetric(
-                vertical: 10.0,
-                horizontal: 8.0,
-              ),
-              child: Row(
-                crossAxisAlignment: CrossAxisAlignment.end,
-                children: <Widget>[
-                  Text.rich(
-                    TextSpan(
-                      children: [
-                        TextSpan(
-                          text: '$goal\n',
-                          style: kGoalTextStyle,
-                        ),
-                        TextSpan(
-                          text: 'in $duration years',
-                          style: kDurationTextStyle,
-                        ),
-                      ],
-                    ),
+            child: Image.asset(
+              'images/clock.jpg',
+              fit: BoxFit.fill,
+              height: 100,
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.symmetric(
+              vertical: 10.0,
+              horizontal: 8.0,
+            ),
+            child: Row(
+              crossAxisAlignment: CrossAxisAlignment.end,
+              children: <Widget>[
+                Text.rich(
+                  TextSpan(
+                    children: [
+                      TextSpan(
+                        text: '$goal\n',
+                        style: kGoalTextStyle,
+                      ),
+                      TextSpan(
+                        text: 'in $duration years',
+                        style: kDurationTextStyle,
+                      ),
+                    ],
                   ),
-                  const Spacer(),
-                  const Icon(
-                    Icons.share,
-                    color: kTextsColor,
-                  ),
-                  const SizedBox(
-                    width: 15.0,
-                  ),
-                  const Icon(
+                ),
+                const Spacer(),
+                const Icon(
+                  Icons.share,
+                  color: kTextsColor,
+                  size: 30.0,
+                ),
+                const SizedBox(
+                  width: 15.0,
+                ),
+                InkWell(
+                  onTap: onTap,
+                  child: const Icon(
                     Icons.check_circle,
                     color: kUnselectedItemColor,
+                    size: 30.0,
                   ),
-                ],
-              ),
+                ),
+              ],
             ),
-          ],
-        ),
+          ),
+        ],
       ),
     );
   }
